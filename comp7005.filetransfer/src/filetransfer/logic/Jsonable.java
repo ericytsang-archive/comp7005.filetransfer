177af1f13a347709947d9a88d6bd1edc454ff986
Eric Tsang <surplus.et@gmail.com>
2015-09-30 22:47:29 -0700

    refined jsonable
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- comp7005.filetransfer/src/filetransfer/logic/Jsonable.java
+++ comp7005.filetransfer/src/filetransfer/logic/Jsonable.java
@@ -2,6 +2,8 @@ package filetransfer.logic;
 
 import org.json.JSONObject;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 /**
  * objects that implement this interface are given the ability to be serialized
  *   to a JSON object, and should provide a user-defined method to parse the
@@ -19,4 +21,19 @@ public interface Jsonable
      *   recreated via a user-defined factory method.
      */
     JSONObject toJsonObject();
+
+    /**
+     * recreates an object from the passed JSON object. implementations of
+     *   this interface need to define their own fromJsonObject static method.
+     *   if they don't then when this is used, it will throw an exception.
+     *
+     * @param json a JSON object to deserialize into the object being returned.
+     *
+     * @return the object recreated from the passed JSON object.
+     */
+    @SuppressWarnings("unused")
+    static Object fromJsonObject(JSONObject json)
+    {
+        throw new NotImplementedException();
+    }
 }
